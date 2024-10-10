f11de13f1d3e04fdced0b10123de6ff1edd60f96
Martin Haas <martin.haas@optabim.com>
2024-10-10 19:16:25 +0200

    commit

---
+++ src/classes/ProjectManager.ts
@@ -0,0 +1,192 @@
+import { Project, IProject } from './Project';
+import { Todo } from './TodoClass';
+
+export class ProjectManager {
+    // interna clsser/property
+    list: Project[] = []; // skapar en ny array med typen Project
+    ui: HTMLDivElement; // skapar en ny variabel som är av typen HTMLDivElement
+    id: string;
+
+    // constructor for ProjectManager class
+    constructor(container: HTMLDivElement) {
+        this.ui = container;
+    }
+
+    newProject(data: IProject) { // skapar en ny metod som tar in data av typen IProject
+        const projectNames = this.list.map((project) => {
+            return project.name;
+        });
+
+        //M2-Assigment Q#3
+        const nameToLong = data.name.length > 5;
+        if (nameToLong) {
+            console.warn("för långt namn");
+            return false;
+        }
+        const nameInUse = projectNames.includes(data.name);
+        if (nameInUse) {
+            throw new Error(data.name + "finns redan"); // skapar en ny error som skickar ut ett meddelande om att namnet redan finns
+        }
+        //M2-Assigment Q#7
+        const project = new Project(data);
+
+        project.ui.addEventListener("click", () => {
+            const projectPage = document.getElementById("project-page") as HTMLDivElement;
+            const detailsPage = document.getElementById("project-details") as HTMLDivElement;
+            if (!detailsPage) { return; }
+
+            projectPage.style.display = "none";
+            detailsPage.style.display = "flex";
+            this.setDetailsPage(project, this.id);
+        });
+
+        this.ui.append(project.ui);
+        this.list.push(project);
+        return project;
+    }
+
+    setDetailsPage(project: Project, _id: string) {
+        const detailsPage = document.getElementById("project-details") as HTMLDivElement;
+        const todoDiv = document.getElementById("Todolist") as HTMLDivElement;
+        this.id = project.id;
+        if (!detailsPage && !todoDiv) { return; }
+        const name = document.querySelector("[data-project-info='name']");
+        const description = document.querySelector("[data-project-description='description']");
+        const role = document.querySelector("[card-project-role='role']");
+        const status = document.querySelector("[card-project-status='card-status']");
+        const date = document.querySelector("[card-data-date='date']");
+
+        //M2-Assignment Q#1
+        const nameIcon = document.querySelector("[class-header-class='dashboard-card-header']");
+
+        if (name && description && role && status && date && nameIcon) {
+            name.textContent = project.name;
+            description.textContent = project.description;
+            role.textContent = project.role;
+            status.textContent = project.status;
+            date.textContent = project.date.toDateString();
+
+            //M2-Assignment Q#1
+            nameIcon.textContent = project.name.substring(0, 2);
+        }
+    }
+
+    //M2-Assignment Q#5
+    getProject(id: string) {
+        const project = this.list.find((project) => {
+            console.log(`Checking project with id: ${project.id}`);
+            return project.id === id;
+        });
+        if (project) {
+            console.log(`Found project with id: ${project.id}`);
+            console.log(project);
+        }
+        return project;
+    }
+
+    //M2-Assignment Q#9
+    getToTodo(id: string) {
+        const todo = this.list.find((todoID) => {
+            //condition to find
+            return todoID.id === id;
+        });
+        if (todo) {
+            console.warn(`Found project with id: ${todo.id}`);
+        }
+    }
+
+    deleteProject(id: string) {
+        const project = this.getProject(id);
+        if (!project) { return; }
+        project.ui.remove();
+
+        const remaining = this.list.filter((project) => {
+            return project.id !== id;
+        });
+        this.list = remaining;
+    }
+
+    getTotalCost() {
+        const totalCost = this.list.reduce((accumulator, project) => accumulator + project.cost, 0);
+        return totalCost;
+    }
+
+    getProjectbyName(id: string) {
+        const projectname = this.list.find((project) => {
+            return project.name === id;
+        });
+    }
+
+    //M2-Assignment Q#6
+    addTodoToProject(todo: Todo, projectId: string) {
+        const project = this.list.find(project =>
+            project.id === projectId);
+
+        if (!project) {
+            throw new Error(`Project with id ${projectId} not found.`);
+        }
+
+        project.addTodo(todo);
+    }
+
+    addProject(project: Project) {
+        this.list.push(project);
+    }
+
+    //M2-Assigment Q#7
+    exportToJSON(fileName: string = "projects") {
+        const json = JSON.stringify(this.list, null, 2);
+        const blob = new Blob([json], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    //M2-Assigment Q#8
+    updateProject(data: IProject) {
+        const project = this.list.find(project => project.id === this.id); //hittar project ID matchar ID i Array
+
+        if (project) {
+            project.name = data.name;
+            project.description = data.description;
+            project.role = data.role;
+            project.status = data.status;
+            project.date = new Date(data.date);
+            
+        } else {
+            throw new Error(`Project with id ${this.id} not found.`);
+        }
+    }
+
+    importJSON(){
+        const input = document.createElement(`input`)
+        input.type = `file`
+        input.accept = `application/json`
+        const reader = new FileReader()
+        reader.addEventListener( "load", () => {
+            const json = reader.result
+            if (!json) {return}
+            const projects: IProject[] = JSON.parse(json as string)
+            
+            for (const project of projects) {
+                const projectUpdate  = this.getProject(this.id)
+                if(projectUpdate) { 
+                    //M2-Assigment Q#8
+                    this.updateProject(project)
+                } else {
+                    this.newProject(project)// "projects" tar in proprties från Class IProject
+                }
+            }   
+        })
+
+        input.addEventListener(`change`, () => {
+            const filelist = input.files
+            if (!filelist) {return}
+            reader.readAsText(filelist[0])
+        })
+        input.click()
+    }
+    }
---
+++ src/index.ts
@@ -0,0 +1,295 @@
+import { IProject, role, status } from "./classes/Project"
+import { ProjectManager } from "./classes/ProjectManager"
+import { closeModal, showModal, toggleModal, } from "./classes/Modal"
+import { Todo, ITodo } from "./classes/TodoClass"
+import { v4 as uuidv4 } from 'uuid';
+
+
+const projectlistUI = document.getElementById("project-list") as HTMLDivElement
+const projectManager = new ProjectManager(projectlistUI)
+
+
+
+// KLickar på knappen "New Project" och skapar en ny div med klassen "project" 
+const newProjectBtn= document.getElementById("new-project-btn")
+if (newProjectBtn) {
+    newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+} else {
+    console.warn("No new project button found")
+}
+
+
+const projectPage = document.getElementById("project-page") as HTMLDivElement;
+const homePageButton = document.getElementById("homebtn") as HTMLButtonElement;
+const todoDiv = document.getElementById("To-dolist") as HTMLDivElement;
+
+if (homePageButton) {
+    homePageButton.addEventListener("click", () => {
+        projectPage.style.display = "flex";
+        todoDiv.style.display = "none";
+    });
+}
+
+
+
+const projectForm = document.getElementById("new-project-form")
+
+if(projectForm && projectForm instanceof HTMLFormElement) {
+    projectForm.addEventListener("submit", (event) => {
+        event.preventDefault() // förhindrar att sidan laddas om
+        const formData = new FormData(projectForm) // skapar en ny instans av FormData
+        const projectProperty: IProject = { // skapar en ny variabel med objektet
+            description: formData.get("description") as string, // hämtar värdet från inputfälten
+            name: formData.get("name") as string,  // hämtar värdet från inputfälten
+            role: formData.get("role") as role,// hämtar värdet från inputfälten och giltigöra att det är av typen role
+            status: formData.get("status") as status, // hämtar värdet från inputfälten och giltigöra att det är av typen status
+            date: new Date (formData.get("date") as string), //
+        }
+        //M2-Assignment Q#4
+        if(!projectForm.date.value) {
+            projectForm.date.value = "2024-03-14"
+        }
+try {
+
+    const project = projectManager.newProject(projectProperty) // skapar en ny variabel som är av typen projectManager och kallar på metoden newProject
+   // nameLength()
+    projectForm.reset() // rensar inputfälten
+    toggleModal ("new-project-modal")
+    console.log(project)
+
+    } catch (error) {
+       const errorElement = document.getElementById("pop-up-modal") as HTMLElement
+        //errorElement.innerHTML  // skapar en ny div med innehåll enligt "pop-up-modal elementet"
+        errorElement.style.display = "flex"; // Visar elementet som normalt är dolt
+        const closeBtnPopup = document.getElementById("close-pop-up-btn")
+        if (closeBtnPopup) {
+          closeBtnPopup.addEventListener("click", () => {
+          errorElement.style.display = "none"; // släcker ner elementet
+        });
+
+        }
+    }
+}) //end of eventlistener
+
+const closeBtn = document.getElementById("close-btn") as HTMLButtonElement
+closeBtn.addEventListener("click", (event) => {closeModal("new-project-modal")})   
+closeModal("new-project-modal")
+console.log(closeBtn)
+
+
+}   else {
+    console.warn("No project form found")
+}
+
+//M2-Assignment Q#7
+const exportBtn = document.getElementById("export-btn")
+if(exportBtn)  {
+        exportBtn.addEventListener("click", () => {
+            projectManager.exportToJSON();
+  })
+}
+
+
+
+const importBtn = document.getElementById("import-btn")
+if(importBtn)  {
+    importBtn.addEventListener("click", () => {
+        projectManager.importJSON()
+    })
+}
+
+
+const editbtn= document.getElementById("edit-button")
+if (editbtn) {
+    editbtn.addEventListener("click", () => {showModal("edit-project-modal")})
+} else {
+    console.warn("No new project button found")
+}
+
+//M2-Assignment Q#5
+const editForm = document.getElementById("edit-project-form") as HTMLFormElement
+if (editForm instanceof HTMLFormElement) {
+    editForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(editForm);
+        /* const projectID =  projectManager.id */
+        const projectToUpdate = projectManager.getProject(projectManager.id)
+
+        if(projectToUpdate) {
+            projectToUpdate.description = formData.get("description") as string,
+            projectToUpdate.name = formData.get("name") as string,  // hämtar värdet från inputfälten
+            projectToUpdate.role = formData.get("role") as role,// hämtar värdet från inputfälten och giltigöra att det är av typen role
+            projectToUpdate.status= formData.get("status") as status, // hämtar värdet från inputfälten och giltigöra att det är av typen status
+            projectToUpdate.date = new Date (formData.get("date") as string) //
+        }
+        
+        try {
+            if (projectToUpdate) {
+                projectManager.setDetailsPage(projectToUpdate, projectManager.id); // Call the setDetailsPage method with the project and project.id
+                projectToUpdate.setUI()
+                editForm.reset(); // Reset the input fields
+                toggleModal("edit-project-modal");
+                console.log(projectToUpdate);        
+            }
+        } catch (error) {
+            // Handle the error
+        }
+
+        const errorElement = document.getElementById("pop-up-modal") as HTMLElement
+        errorElement.style.display = "flex"; // Visar elementet som normalt är dolt
+
+        const closeBtnPopup = document.getElementById("close-pop-up-btn")
+        if (closeBtnPopup) {
+            closeBtnPopup.addEventListener("click", () => {
+                errorElement.style.display = "none"; // släcker ner elementet
+            });
+        }
+
+    })
+};
+
+const closeBtn = document.getElementById("edit-close-btn") as HTMLButtonElement
+closeBtn.addEventListener("click", (event) => {closeModal("edit-project-modal")})   
+closeModal("edit-project-modal")
+console.log(closeBtn)
+
+const toDoListDivElement = document.getElementById("Todolist") as HTMLDivElement;
+function colorChangeStatus (status:status, div:HTMLDivElement) {
+    const statusColorMap = {
+        pending: "red",
+        closed: "green",
+        archived: "red",
+    };
+    
+    const color = statusColorMap[status] || "white";
+    div.style.backgroundColor = color
+}
+
+
+
+
+//M2-Assigment-Q#6 ADD TODO
+const addTodo= document.getElementById("addTodo") as HTMLButtonElement
+if (addTodo) {
+    addTodo.addEventListener("click", () => {showModal("T-Do-project-modal")})
+} else {
+    console.warn("No new project button found")
+}
+//M2-Assigment-Q#6
+const todDoForm = document.getElementById("T-Do-project-form") as HTMLFormElement
+    
+    if(todDoForm instanceof HTMLFormElement ) {
+
+        todDoForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(todDoForm);
+        
+   /*      if (getTodo) {
+            getTodo.description = formData.get("description-todo") as string;
+            getTodo.name = formData.get("name-todo") as string;
+            getTodo.status = formData.get("status") as status;
+            getTodo.date = new Date(formData.get("date") as string);
+            getTodo.id = uuidv4();
+        }  */
+        const todoData: ITodo = {
+            id: uuidv4(),
+            name: formData.get("name-todo") as string,
+            description: formData.get("description-todo") as string,
+            status: formData.get("status") as status,
+            date : new Date(formData.get("date") as string).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                
+                
+            })
+        }
+   
+        
+        // querySelect the div with class "To-dolist" 
+        const toDoListDiv = document.querySelector(".Todolist");
+        //Create new aa div Element
+        if (toDoListDiv) {
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `
+            <div class="Todolist" id="todo-list" style="display: flex";>
+                <h4 name="name-todo" class="T-doHeader">${todoData.name}</h4>
+                <p1 id="description-todo" name="description-todo">${todoData.description}</p1>
+            <div id="date-todo">${todoData.date}</div>
+                <div id="todostatus" name="todostatus">${todoData.status}</div>
+            </div>
+            ` ;
+            newDiv.style.display = "flex"; 
+            // colorChangeStatus(todoData.status as status, toDoListDivElement )
+            // qeuerySelect the container within the .dashboard-card div to which you want to append the new div
+            const container = document.querySelector(".dashboard-card-todo");
+            // Append the new div to the container
+            if (container) {
+                container.appendChild(newDiv);
+            } 
+            
+            //M2-Assigment-Q#9
+            if (todoData.status === "Pending") {
+                newDiv.style.backgroundColor = "yellow";
+                newDiv.style.color = "black";
+            } else if (todoData.status === "Closed") { 
+                newDiv.style.backgroundColor = "red";
+            } else {
+                newDiv.style.color = "white";
+            }
+            
+        
+        }
+        
+        // Toggle the modal
+        toggleModal("T-Do-project-modal");
+        todDoForm.reset();
+
+        //Using the addtodoproject to push it to an array property in Project Class
+        const todo = todoData;
+        const todoID = todoData.id ;
+        const project = projectManager.id;
+        const projectToUpdate = projectManager.getProject(project);
+        
+        const editTodoBtn = document.getElementById("editTodbtn") as HTMLButtonElement;
+        const edittodoForm = document.getElementById("edittodoform") as HTMLFormElement;  
+        
+        if (edittodoForm instanceof HTMLFormElement) {
+        editTodoBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            const formData = new FormData(editForm);
+            const todoToUpdate = projectManager.getProject(todoID);
+            console.log(todoToUpdate);
+
+            if(todoToUpdate) {
+                todoToUpdate.description = formData.get("description-todo") as string;
+                todoToUpdate.name = formData.get("name-todo") as string;
+                todoToUpdate.status = formData.get("status") as status;
+                todoToUpdate.date = new Date(formData.get("date") as string);
+            }
+            try {
+                if (todoToUpdate) {
+                    projectManager.setDetailsPage(todoToUpdate, todoID);
+                    todoToUpdate.setUI();
+                    edittodoForm.reset();
+                    toggleModal("editToDoprojectmodal");
+                    console.log(todoToUpdate);
+                }
+
+            showModal("editToDoprojectmodal");
+        } catch (error) {
+            // Handle the error
+        }
+       
+        
+        if (projectToUpdate) {
+            projectManager.addTodoToProject(todo, project);
+            projectToUpdate.setUI();
+            console.log(projectToUpdate);
+        }
+    })
+    }
+ }
+); 
+}
